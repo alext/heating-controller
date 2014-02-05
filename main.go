f05c59d175d9cf96321e4931ff96841799a14237
Alex Tomlins <alex@tomlins.org.uk>
2014-02-05 21:59:43 +0000

    Main function to start server with one output
    Also commented out toggle loop

--- main.go
+++ main.go
@@ -13,32 +13,57 @@ var port = flag.Int("port", 8080, "The port to listen on")
 func main() {
 	flag.Parse()
 
-	output, err := output.New("ch", 22)
+	//toggleLoop()
+
+	srv := webserver.New(*port)
+	out, err := output.New("ch", 22)
+	if err != nil {
+		log.Fatal("Error creating output: ", err)
+	}
+	srv.AddOutput(out)
+	err = srv.Run()
+	if err != nil {
+		log.Fatal("Server.Run: ", err)
+	}
+}
+
+func toggleLoop() {
+	out, err := output.New("ch", 22)
 	if err != nil {
 		log.Fatal("Error creating output:", err)
 	}
 	for ; true ; {
 		log.Print("Activating output")
-		err = output.Activate()
+		err = out.Activate()
 		if err != nil {
 			log.Fatal("Error activating output:", err)
 		}
+		state, err := out.Active()
+		if err != nil {
+			log.Fatal("Error reading state:", err)
+		}
+		log.Printf("  Current state: %v", state)
+		state, err = out.Active()
+		if err != nil {
+			log.Fatal("Error reading state:", err)
+		}
+		log.Printf("  Current state: %v", state)
 
 		log.Print("   sleeping...")
 		time.Sleep(5 * time.Second)
 
 		log.Print("Deactivating output")
-		err = output.Deactivate()
+		err = out.Deactivate()
 		if err != nil {
 			log.Fatal("Error deactivating output:", err)
 		}
+		state, err = out.Active()
+		if err != nil {
+			log.Fatal("Error reading state:", err)
+		}
+		log.Printf("  Current state: %v", state)
 
 		log.Print("   sleeping...")
 		time.Sleep(5 * time.Second)
-	}
-	srv := webserver.New(*port)
-	err = srv.Run()
-	if err != nil {
-		log.Fatal("Server.Run: ", err)
 	}
 }
