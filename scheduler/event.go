03c78549e050e7df50bdff7f333d888b7f3b2b3a
Alex Tomlins <alex@tomlins.org.uk>
2017-02-05 18:19:01 +0000

    Event can specify weekdays it applies on.
    The first stage of 7 day schedules, allowing events to be specified as
    only applying to some days of the week.

--- scheduler/event.go
+++ scheduler/event.go
@@ -5,10 +5,21 @@ import (
 	"time"
 )
 
+const (
+	Sunday    uint8 = 1 << uint8(time.Sunday)
+	Monday    uint8 = 1 << uint8(time.Monday)
+	Tuesday   uint8 = 1 << uint8(time.Tuesday)
+	Wednesday uint8 = 1 << uint8(time.Wednesday)
+	Thursday  uint8 = 1 << uint8(time.Thursday)
+	Friday    uint8 = 1 << uint8(time.Friday)
+	Saturday  uint8 = 1 << uint8(time.Saturday)
+)
+
 type Event struct {
 	Hour   int    `json:"hour"`
 	Min    int    `json:"min"`
 	Action Action `json:"action"`
+	Days   uint8  `json:"days"`
 }
 
 func (e Event) NextOccurance() time.Time {
@@ -26,6 +37,14 @@ func (e Event) nextOccuranceAfter(current time.Time) time.Time {
 		next = time.Date(current.Year(), current.Month(), current.Day(), e.Hour, e.Min, 0, 0, time.Local)
 	}
 	return next
+}
+
+func (e Event) ActiveOn(day time.Weekday) bool {
+	if e.Days == 0 {
+		// No days set, always active
+		return true
+	}
+	return (1<<uint8(day))&e.Days > 0
 }
 
 func (e Event) after(hour, min int) bool {
--- scheduler/event_test.go
+++ scheduler/event_test.go
@@ -1,6 +1,8 @@
 package scheduler
 
 import (
+	"time"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
@@ -25,6 +27,32 @@ var _ = Describe("Event", func() {
 
 		It("should be invalid for an minute greater than 59", func() {
 			Expect(Event{Min: 60}.Valid()).To(BeFalse())
+		})
+	})
+
+	Describe("Applying to specific days only", func() {
+		var e Event
+
+		It("applies to all days by default", func() {
+			for i := 0; i < 7; i++ {
+				day := time.Weekday(i)
+				Expect(e.ActiveOn(day)).To(BeTrue(),
+					"Expected event to be active on "+day.String())
+			}
+		})
+
+		It("is only active on the specified days", func() {
+			e.Days = Monday | Thursday
+
+			Expect(e.ActiveOn(time.Monday)).To(BeTrue(),
+				"Expected event to be active on Monday")
+			Expect(e.ActiveOn(time.Thursday)).To(BeTrue(),
+				"Expected event to be active on Thursday")
+
+			Expect(e.ActiveOn(time.Sunday)).To(BeFalse(),
+				"Expected event not to be active on Sunday")
+			Expect(e.ActiveOn(time.Tuesday)).To(BeFalse(),
+				"Expected event not to be active on Tuesday")
 		})
 	})
 })
