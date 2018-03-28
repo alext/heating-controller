d242d911d934bad5d38c5395775748c2430a3ddd
Alex Tomlins <alex@tomlins.org.uk>
2018-03-28 15:18:03 +0100

    Split out zone demand tests to separate file
    The tests were quite long, and I'm about to add more tests, so it makes
    sense to split this area out.
    
    With them split, there's no need for the main tests to use the controller
    package, they only need to use the exported identifiers.

---
+++ controller/zone_demand_test.go
@@ -0,0 +1,170 @@
+package controller
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/extensions/table"
+	. "github.com/onsi/gomega"
+
+	"github.com/alext/heating-controller/output"
+	"github.com/alext/heating-controller/scheduler"
+)
+
+var _ = Describe("Zone demand handling", func() {
+
+	Describe("handling scheduler demand", func() {
+		var (
+			out output.Output
+			z   *Zone
+		)
+
+		BeforeEach(func() {
+			out = output.Virtual("something")
+			z = NewZone("someting", out)
+		})
+
+		Describe("activating demand", func() {
+			BeforeEach(func() {
+				out.Deactivate()
+				z.schedDemand = false
+
+				z.schedulerDemand(scheduler.TurnOn)
+			})
+
+			It("should update the demand state", func() {
+				Expect(z.schedDemand).To(BeTrue())
+			})
+
+			It("should trigger an update to overall demand", func() {
+				Expect(out.Active()).To(BeTrue())
+			})
+		})
+
+		Describe("deactivating demand", func() {
+			BeforeEach(func() {
+				out.Activate()
+				z.schedDemand = true
+				z.currentDemand = true // force the output to be updated
+
+				z.schedulerDemand(scheduler.TurnOff)
+			})
+
+			It("should update the demand state", func() {
+				Expect(z.schedDemand).To(BeFalse())
+			})
+
+			It("should trigger an update to overall demand", func() {
+				Expect(out.Active()).To(BeFalse())
+			})
+		})
+	})
+
+	Describe("handling thermostat demand", func() {
+		var (
+			out output.Output
+			z   *Zone
+		)
+
+		BeforeEach(func() {
+			out = output.Virtual("something")
+			z = NewZone("someting", out)
+			z.schedDemand = true // so that thermDemand changes trigger output
+		})
+
+		Describe("activating demand", func() {
+			BeforeEach(func() {
+				out.Deactivate()
+				z.thermDemand = false
+
+				z.thermostatDemand(true)
+			})
+
+			It("should update the demand state", func() {
+				Expect(z.thermDemand).To(BeTrue())
+			})
+
+			It("should trigger an update to overall demand", func() {
+				Expect(out.Active()).To(BeTrue())
+			})
+		})
+
+		Describe("deactivating demand", func() {
+			BeforeEach(func() {
+				out.Activate()
+				z.thermDemand = true
+				z.currentDemand = true // force the output to be updated
+
+				z.thermostatDemand(false)
+			})
+
+			It("should update the demand state", func() {
+				Expect(z.thermDemand).To(BeFalse())
+			})
+
+			It("should trigger an update to overall demand", func() {
+				Expect(out.Active()).To(BeFalse())
+			})
+		})
+	})
+
+	type demandCase struct {
+		schedDemand    bool
+		thermDemand    bool
+		currentDemand  bool
+		expectedDemand bool
+
+		initialOutputState  bool
+		expectedOutputState bool
+	}
+
+	DescribeTable("applying changes in demand state",
+		func(c demandCase) {
+			out := output.Virtual("something")
+			if c.initialOutputState {
+				out.Activate()
+			} else {
+				out.Deactivate()
+			}
+			z := &Zone{
+				Out:           out,
+				schedDemand:   c.schedDemand,
+				thermDemand:   c.thermDemand,
+				currentDemand: c.currentDemand,
+			}
+			z.updateDemand()
+			Expect(z.currentDemand).To(Equal(c.expectedDemand))
+			Expect(out.Active()).To(Equal(c.expectedOutputState))
+		},
+		Entry("activates when both demands active", demandCase{
+			schedDemand: true, thermDemand: true, currentDemand: false,
+			expectedDemand:     true,
+			initialOutputState: false, expectedOutputState: true,
+		}),
+		Entry("deactivates when only scheduler demand active", demandCase{
+			schedDemand: true, thermDemand: false, currentDemand: true,
+			expectedDemand:     false,
+			initialOutputState: true, expectedOutputState: false,
+		}),
+		Entry("deactivates when only thermostat demand active", demandCase{
+			schedDemand: false, thermDemand: true, currentDemand: true,
+			expectedDemand:     false,
+			initialOutputState: true, expectedOutputState: false,
+		}),
+		Entry("deactivates when neither demand active", demandCase{
+			schedDemand: false, thermDemand: false, currentDemand: true,
+			expectedDemand:     false,
+			initialOutputState: true, expectedOutputState: false,
+		}),
+		Entry("does not change output when demand already active", demandCase{
+			schedDemand: true, thermDemand: true, currentDemand: true,
+			expectedDemand: true,
+			// Set to false to ensure output doesn't get called.
+			initialOutputState: false, expectedOutputState: false,
+		}),
+		Entry("does not change output when demand already inactive", demandCase{
+			schedDemand: false, thermDemand: false, currentDemand: false,
+			expectedDemand: false,
+			// Set to true to ensure output doesn't get called.
+			initialOutputState: true, expectedOutputState: true,
+		}),
+	)
+})
